c8f7b4845ce74536fbdcee3c56d2c291f20ddeee
Luca Hagspiel <luca.hagspiel@gmail.com>
2025-10-07 12:16:58 +0200

    Arrayfuntions und TS

--- web_basics/TypeScript/2. Arrayfunctions-und-TS/script.ts
+++ web_basics/TypeScript/2. Arrayfunctions-und-TS/script.ts
@@ -1,7 +1,7 @@
 interface Person { id: number; name: string; age: number; active: boolean }
 
-const nums: number[] = [5, 2, 9, 1, 5, 6];
-const people: Person[] = [
+let nums: number[] = [5, 2, 9, 1, 5, 6];
+let people: Person[] = [
     { id: 1, name: "Anna", age: 28, active: true },
     { id: 2, name: "Ben",  age: 17, active: false },
     { id: 3, name: "Cara", age: 34, active: true },
@@ -9,29 +9,29 @@ const people: Person[] = [
 ];
 
 // 1) map
-const doubled: number[] = nums.map((n: number): number => n * 2);
+let doubled: number[] = nums.map((n: number): number => n * 2);
 console.log("map - doubled:", doubled);
 
 // 2) filter
-const adults: Person[] = people.filter((p: Person): boolean => p.age >= 18);
+let adults: Person[] = people.filter((p: Person): boolean => p.age >= 18);
 console.log("filter - adults:", adults.map(p => p.name));
 
 // 3) sort
-const peopleByAge: Person[] = [...people].sort((a: Person, b: Person): number => a.age - b.age);
+let peopleByAge: Person[] = [...people].sort((a: Person, b: Person): number => a.age - b.age);
 console.log("sort - people by age:", peopleByAge.map(p => `${p.name}(${p.age})`));
 
 // 4) find
-const firstTeen: Person | undefined = people.find((p: Person): boolean => p.age < 20);
+let firstTeen: Person | undefined = people.find((p: Person): boolean => p.age < 20);
 console.log("find - first teen:", firstTeen);
 
 // 5) reduce
-const sum: number = nums.reduce((acc: number, cur: number): number => acc + cur, 0);
+let sum: number = nums.reduce((sum: number, cur: number): number => sum + cur, 0);
 console.log("reduce - sum:", sum);
 
 // 6) some
-const hasMinor: boolean = people.some((p: Person): boolean => p.age < 18);
+let hasMinor: boolean = people.some((p: Person): boolean => p.age < 18);
 console.log("some - has minor:", hasMinor);
 
 // 7) every
-const allAdults: boolean = people.every((p: Person): boolean => p.age >= 18);
+let allAdults: boolean = people.every((p: Person): boolean => p.age >= 18);
 console.log("every - all adults:", allAdults);
