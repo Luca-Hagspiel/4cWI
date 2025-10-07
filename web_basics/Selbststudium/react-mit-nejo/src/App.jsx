96bc51685bd54eefd15c99327e42c56d4b966a01
Luca Hagspiel <luca.hagspiel@gmail.com>
2025-10-07 10:20:59 +0200

    7. Aufgabe Responsive mit Tailwind

--- web_basics/Selbststudium/react-mit-nejo/src/App.jsx
+++ web_basics/Selbststudium/react-mit-nejo/src/App.jsx
@@ -1,7 +1,9 @@
+import List from "./components/List";
+
 export default function App() {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
+        <div className="m-4">
+            <List items={["Coffee", "Tea", "Beer", "Water"]} />
+        </div>
+    );
 }
