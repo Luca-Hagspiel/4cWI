8c12ef335eb055eb0fe6233d3d8e05f7024b21c9
Luca Hagspiel <luca.hagspiel@gmail.com>
2025-10-14 11:22:56 +0200

    Aufgabe 3 TS OO

--- web_basics/TypeScript/3. TS-OO/main.ts
+++ web_basics/TypeScript/3. TS-OO/main.ts
@@ -9,3 +9,7 @@ console.log(Engine1.getHersteller());
 console.log(Engine1.getHorsepower());
 console.log(Engine1.getKraftstoffverbrauch());
 console.log(Engine1.getBesitzer());
+
+let Besitzer2: Besitzer = new Besitzer("Erika", "Mustermann", 38);
+Engine1.setBesitzer(Besitzer2);
+console.log(Engine1.getBesitzer());
